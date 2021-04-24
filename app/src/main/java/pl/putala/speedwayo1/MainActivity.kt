1b92628a08c5a6f0ae00ca4978e5865bca34c978
Mateusz <mateuszputala@gmail.com>
2021-04-24 14:06:35 +0200

    First button

--- app/src/main/java/pl/putala/speedwayo1/MainActivity.kt
+++ app/src/main/java/pl/putala/speedwayo1/MainActivity.kt
@@ -2,10 +2,26 @@ package pl.putala.speedwayo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonRegister = findViewById<Button>(R.id.buttonRegister)
+        val buttonSingIn = findViewById<Button>(R.id.buttonSingIn)
+        buttonRegister.setOnClickListener {
+            buttonRegister.text = "Akcja!!!!";
+            Toast.makeText(applicationContext, "Teraz się rejestrujesz.", Toast.LENGTH_SHORT).show()
+        }
+        buttonSingIn.setOnClickListener {
+            buttonSingIn.text = "Kupa!!!!";
+            Toast.makeText(applicationContext,"Teraz się logujesz.", Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/pl/putala/speedwayo1/Matches.kt
@@ -0,0 +1,11 @@
+package pl.putala.speedwayo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Matches : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_matches)
+    }
+}
