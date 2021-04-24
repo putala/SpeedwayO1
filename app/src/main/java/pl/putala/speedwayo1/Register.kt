cbf01f1287d08870d8c4851ccac916fd103c5bfe
Mateusz <mateuszputala@gmail.com>
2021-04-24 12:57:02 +0200

    activity register

---
+++ app/src/main/java/pl/putala/speedwayo1/Register.kt
@@ -0,0 +1,11 @@
+package pl.putala.speedwayo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
